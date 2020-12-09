5e7ab30734a69b389fd37f93fc463f0f89cdeeaa
sagarrana317 <75519500+sagarrana317@users.noreply.github.com>
2020-12-09 12:27:11 +0530

    Add files via upload

---
+++ rana.py
@@ -0,0 +1,20 @@
+samsung=50000
+apple=70000
+lenovo=60000
+asus=80000
+laptop=input("which laptop you wana buy ")
+if laptop=="samsung":
+    print("pay rs",samsung)
+if laptop=="apple":
+    print("pay rs",apple)
+if laptop=="lenovo":
+    print("pay rs",lenovo)
+if laptop=="asus":
+    print("pay rs",asus)
+
+else:
+    print("sorry this is not ava",laptop)
+    
+
+
+
